df3f78827e3b953687b7340b2922444f5a046e74
PriyaM <priyamalaghan31@gmail.com>
2023-07-10 10:55:59 -0400

    EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys #Sys module in python provides various functions and variables that are used to manipulate different parts of th epython runtime environment
-import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -20,5 +20,5 @@ class CustomException(Exception):
     try:
         a=1/0
     except Exception as e:
-        #logging.info("Divide by zero error")
+        logging.info("Divide by zero error.")
         raise CustomException(e,sys)'''
